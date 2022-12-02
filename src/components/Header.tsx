e95b4547015e456f2ef315c60a1e28537b222c0d
vovitolog <vovitolog@gmail.com>
2022-12-02 07:04:36 +0300

    refactor: split code into components

---
+++ src/components/Header.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <header className={'header'}>
+            <img src={'https://i.pinimg.com/564x/4a/d4/9f/4ad49ffc017cb57a2ec50ecb69e40fa7.jpg'}/>
+        </header>
+    );
+};
+
+export default Header;
---
+++ src/components/Profile.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const Profile = () => {
+    return (
+        <div className={'content'}>
+            <img
+                src={'https://images.unsplash.com/photo-1519389950473-47ba0277781c?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1740&q=80'}/>
+            Main Content
+            <div>
+                avatar
+                <img className={'avatar'}
+                     src={'https://cdn1.vectorstock.com/i/1000x1000/38/05/male-face-avatar-logo-template-pictograph-vector-11333805.jpg'}/>
+            </div>
+        </div>
+    );
+};
+
+export default Profile;
