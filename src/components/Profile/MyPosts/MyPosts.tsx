4cd902a6f3b330966bf4e064ea5bf45cfb77bcff
vovitolog <vovitolog@gmail.com>
2022-12-03 11:09:42 +0300

    refactor: add Post and MyPosts components

---
+++ src/components/Profile/MyPosts/MyPosts.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import classes from './MyPosts.module.css'
+import Post from "./Post/Post";
+
+const MyPosts = () => {
+    return (
+        <div className={classes.posts}>
+            My posts
+            <div>
+                New post
+            </div>
+            <Post/>
+            <Post/>
+        </div>
+    );
+};
+
+export default MyPosts;
---
+++ src/components/Profile/MyPosts/Post/Post.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import classes from './Post.module.css'
+
+const Post = () => {
+    return (
+        <div className={classes.item}>
+            post 1
+        </div>
+    );
+};
+
+export default Post;
