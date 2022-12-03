282ae4c2b8f61c9641a290625490be7067da62e1
vovitolog <vovitolog@gmail.com>
2022-12-03 14:46:34 +0300

    feat: add Dialogs layout

--- src/components/Dialogs/Dialogs.tsx
+++ src/components/Dialogs/Dialogs.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import classes from 'Dialogs.module.css'
+import classes from './Dialogs.module.css'
 
 type DialogsProps = {
 
@@ -7,8 +7,17 @@ type DialogsProps = {
 
 const Dialogs = (props: DialogsProps) => {
     return (
-        <div>
-            test
+        <div className={classes.dialogs}>
+            <div className={classes.dialogItems}>
+                <div className={classes.dialog + ' '+ classes.active}>Nick</div>
+                <div className={classes.dialog}>Steve</div>
+                <div className={classes.dialog}>Brady</div>
+            </div>
+            <div className={classes.messages}>
+                <div className={classes.message}>Hi</div>
+                <div className={classes.message}>How are You</div>
+                <div className={classes.message}>Bye</div>
+            </div>
         </div>
     );
 };
