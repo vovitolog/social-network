330b71500909faa84cf65b091b8b61c455fbc31b
vovitolog <vovitolog@gmail.com>
2022-12-03 10:25:46 +0300

    refactor: add css modules files

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,8 +1,9 @@
 import React from 'react';
+import classes from './Header.module.css'
 
 const Header = () => {
     return (
-        <header className={'header'}>
+        <header className={classes.header}>
             <img src={'https://i.pinimg.com/564x/4a/d4/9f/4ad49ffc017cb57a2ec50ecb69e40fa7.jpg'}/>
         </header>
     );
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,8 +1,9 @@
 import React from 'react';
+import classes from './Navbar.module.css'
 
 const Navbar = () => {
     return (
-        <nav className={'nav'}>
+        <nav className={classes.nav}>
             <div>
                 <a>Profile</a>
             </div>
