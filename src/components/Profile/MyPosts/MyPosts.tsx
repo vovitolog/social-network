756521fed755a03ee11f9bd133d15db127a85691
vovitolog <vovitolog@gmail.com>
2022-12-11 10:27:24 +0300

    feat: add useRef implementation on input

--- src/components/Profile/MyPosts/MyPosts.tsx
+++ src/components/Profile/MyPosts/MyPosts.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {LegacyRef, TextareaHTMLAttributes, useRef} from 'react';
 import classes from './MyPosts.module.css'
 import Post, {PostProps} from "./Post/Post";
 
@@ -10,14 +10,21 @@ const MyPosts: React.FC<MyPostsProps> = (props) => {
 
     let postsElements = props.posts.map(p => <Post id={p.id} message={p.message} key={p.id}/>)
 
+    //const inputText = React.createRef<HTMLTextAreaElement>();
+    const inputText = useRef<HTMLTextAreaElement>(null);
+
+    const buttonHandler = () => {
+        alert(inputText.current?.value)
+    }
+
     return (
         <div className={classes.postsBlock}>
             <h3>My posts</h3>
             <div>
                 New post
-                <div><textarea></textarea></div>
+                <div><textarea ref={inputText}></textarea></div>
                 <div>
-                    <button>Add Post</button>
+                    <button onClick={buttonHandler}>Add Post</button>
                 </div>
             </div>
             {postsElements}
